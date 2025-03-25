172e06564824af9150b09e9a1deb8924a82f7909
nicolasgoyena <nicolasgoyenaserveto@gmail.com>
2025-03-25 12:15:25 +0100

    Update app.py

--- app.py
+++ app.py
@@ -636,9 +636,11 @@ with tab2:
 
                 if st.button("Calcular y mostrar resultados"):
                     spinner_placeholder = st.empty()
-                    spinner_placeholder.spinner("Calculando fechas disponibles...")
-                    available_dates = get_available_dates(aoi, start_date, end_date, max_cloud_percentage)
-                    spinner_placeholder.empty()
+                    with spinner_placeholder.container():
+                        with st.spinner("🌀 Calculando fechas disponibles..."):
+                            available_dates = get_available_dates(aoi, start_date, end_date, max_cloud_percentage)
+
+                    spinner_placeholder.empty() 
                     if not available_dates:
                         st.warning("⚠️ No se encontraron imágenes dentro del rango de fechas y porcentaje de nubosidad seleccionado.")
                         st.session_state["data_time"] = []
