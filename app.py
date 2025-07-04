d652a67d2f19dd46b5082b7ae86df1253d5b728e
nicolasgoyena <nicolasgoyenaserveto@gmail.com>
2025-07-04 10:32:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -522,17 +522,11 @@ def process_sentinel2(aoi, selected_date, max_cloud_percentage, selected_indices
             "B5_div_B4": lambda: b5.divide(b4).updateMask(cloud_mask).rename('B5_div_B4'),
             "NDCI_ind": lambda: b5.subtract(b4).divide(b5.add(b4)).updateMask(cloud_mask).rename('NDCI_ind'),
             "PC_Val_cal": lambda: (
-                ee.Image(49.14)
-                .divide(
-                    ee.Image(1).add(
-                        (b5.divide(b4).subtract(1.64)).multiply(-6.76).exp()
-                    )
-                )
+                b5.divide(b4).pow(4.5351).multiply(1.2163)
                 .max(0)
                 .updateMask(cloud_mask)
                 .rename("PC_Val_cal")
             ),
-
             "Chla_Val_cal": lambda: (
                 b5.subtract(b4).divide(b5.add(b4)).multiply(5.05).exp().multiply(23.16)
                 .max(0)
