f7a8fa9193d414cd618f4774c9185b6918746d18
nicolasgoyena <nicolasgoyenaserveto@gmail.com>
2025-08-06 14:14:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1119,7 +1119,7 @@ with tab2:
     st.markdown("<hr style='border: 1px solid #b4a89b; margin: 2rem 0;'>", unsafe_allow_html=True)
         
                 # Procesamiento solo si se pulsa
-                if calcular:     
+    if calcular:     
                     with st.spinner("Calculando fechas disponibles..."):
                         usar_csv_val = reservoir_name.lower() == "val" and int(max_cloud_percentage) == 60
                         usar_csv_bellus = reservoir_name.lower() == "bellus" and int(max_cloud_percentage) == 60
@@ -1968,6 +1968,7 @@ with tab4:
                                         if not df_medias.empty:
                                             st.markdown("### 💧 Datos de medias del embalse")
                                             st.dataframe(df_medias.reset_index(drop=True))
+
 
 
 
