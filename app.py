9c847d90d7dbd4e38f69175b8c86a37bc433e780
nicolasgoyena <nicolasgoyenaserveto@gmail.com>
2025-08-06 13:30:51 +0200

    Update app.py

--- app.py
+++ app.py
@@ -866,7 +866,7 @@ with col3:
     st.markdown(
         """
         <div style="margin-left: 25px;">
-            <img src="images/logo_bioma.jpg" width="300">
+            <img src="images/logo_bioma.png" width="300">
         </div>
         """,
         unsafe_allow_html=True
@@ -1966,6 +1966,7 @@ with tab4:
                                         if not df_medias.empty:
                                             st.markdown("### 💧 Datos de medias del embalse")
                                             st.dataframe(df_medias.reset_index(drop=True))
+
 
 
 
